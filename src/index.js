c4fb536d289d38ba8c18b733cdca2c711809771b
TundeKrisztina <64001799+TundeKrisztina@users.noreply.github.com>
2022-08-22 15:15:44 +0200

    showing the current temperature

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "11af92cd97a626ecc5ce2d392902cc0b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
